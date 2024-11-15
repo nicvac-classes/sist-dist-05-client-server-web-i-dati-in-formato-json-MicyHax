0f2a6200fe7dcc0c8f4c7d6baabf94e3a02981e0
Michele <148053642+MicyHax@users.noreply.github.com>
2024-11-15 13:10:04 +0000

    aggiunte

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 
-// INSERIRE QUI IL CODICE INDICATO NELL'ESERCIZIO
+const dataPath = path.join('backend', 'data', 'studenti.json');
+
+function leggiJsonStudenti() {
+    try {
+        const data = fs.readFileSync(dataPath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Errore lettura file:', error);
+        return { studenti: [] };
+    }
+}
+
+function salvaJsonStudenti(data) {
+    try {
+        //I parametri null, 4 permettono di scrivere il file json con
+        // l'indentazione, in modo che sia facilmente leggibile.
+        fs.writeFileSync(dataPath, JSON.stringify(data, null, 4));
+        return true;
+    } catch (error) {
+        console.error('Errore scrittura file:', error);
+        return false;
+    }
+}
+
+app.get('/', (req, res) => {
+    const data = leggiJsonStudenti();
+    res.render('index', { studenti: data.studenti });
+});
+
+// Route per aggiungere studente
+app.post('/aggiungi-studente', (req, res) => {
+    // Leggi dati esistenti
+    const data = leggiJsonStudenti();
+    
+    // Calcola nuovo ID, aggiungendo 1 al valore più grande
+    let nuovoId = 1;
+    let iMax = 0;
+    for (let i = 0; i < data.studenti.length; i++) {
+        if (data.studenti[i].id > data.studenti[iMax].id) {
+            iMax = i;
+            nuovoId = data.studenti[iMax].id + 1;
+        }
+    }
+    
+    // Crea nuovo studente
+    const nuovoStudente = {
+        id: nuovoId,
+        nome: req.body.nome,
+        crediti: parseInt(req.body.crediti)
+    };
+    
+    // Aggiungi il nuovo studente alla struttura e lo salva in JSON
+    data.studenti.push(nuovoStudente);
+    salvaJsonStudenti(data);
+    
+    // Redirect alla home
+    res.redirect('/');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
